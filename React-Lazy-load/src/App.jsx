9e1bf8f522607bfc67001369ad508f2312d9eac8
NieamulKabir <kabir000079@gmail.com>
2024-03-01 00:27:34 +0600

    lazy load update

--- React-Lazy-load/src/App.jsx
+++ React-Lazy-load/src/App.jsx
@@ -1,9 +1,43 @@
-function App() {
-  return (
-    <div>
-      <h1>react lazy loader</h1>
-    </div>
-  );
-}
+import { Suspense, useState } from "react";
+import demos from "./data/demos";
+import importDemo from "./utils/importDemo";
 
-export default App;
+export default function App() {
+    const [selectedDemo, setSelectedDemo] = useState(null);
+
+    const loadDemo = async (file) => {
+        const Demo = await importDemo(file);
+        return <Demo />;
+    };
+
+    const selectDemo = async (file) => {
+        const DemoComponent = await loadDemo(file);
+
+        setSelectedDemo(DemoComponent);
+    };
+
+    return (
+        <div>
+            <div>
+                <h1>React Lazy load explained</h1>
+
+                <div>
+                    {demos.map((demo) => (
+                        <button
+                            key={demo.name}
+                            onClick={() => selectDemo(demo.file)}
+                        >
+                            {demo.name}
+                        </button>
+                    ))}
+                </div>
+
+                <div>
+                    <Suspense fallback={<h1>Loading component...</h1>}>
+                        {selectedDemo}
+                    </Suspense>
+                </div>
+            </div>
+        </div>
+    );
+}
